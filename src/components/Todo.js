ff114ef2362f364cfbee0dd90a25f99feef58817
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-13 20:08:37 -0400

    handling events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,17 @@
-function Todo(props) {
+const Todo = (props) => {
+  const handleDelete = () => {
+    console.log("this is:", props.text);
+  };
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default Todo;
